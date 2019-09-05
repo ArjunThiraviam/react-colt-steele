079b2b1702cd5100cb778b94178a3862e14adb6d
ArjunThiraviam <arjunthiraviam@gmail.com>
2019-09-05 12:12:09 +0530

    CRA and Modules

---
+++ 04-create-react-app/test-app/src/helper.jsx
@@ -0,0 +1,15 @@
+function helpful() {
+    console.log("I did a super helpful thing");
+}
+
+function sort() {
+    console.log("All sorted");
+}
+
+function sing() {
+    console.log("La La La");
+}
+
+
+export default helpful;
+export { sort, sing }
