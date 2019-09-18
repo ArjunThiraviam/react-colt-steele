cb23f2f0a6a6480a6ea30397854f4dec9dd37c1c
ArjunThiraviam <arjunthiraviam@gmail.com>
2019-09-18 12:36:44 +0530

    State intro

---
+++ 06-State/state-demo/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Game from './Game.jsx';
+
+class App extends Component {
+  render() {
+    return <div className="App">
+      <Game />
+    </div>;
+
+  }
+}
+
+export default App;
