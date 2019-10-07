96a09aeeec47056afc040df63c8b92bbf429d846
ArjunThiraviam <arjunthiraviam@gmail.com>
2019-10-07 22:49:52 +0530

    Completed State Demo

--- 06-State/state-demo/src/App.js
+++ 06-State/state-demo/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Game from './Game.jsx';
+import Rando from './Rando.jsx';
+import Button from './Button.jsx';
+import BrokenButton from './BrokenButton.jsx';
+import BrokenClick2 from './BrokenClick2.jsx';
 
 class App extends Component {
   render() {
     return <div className="App">
-      <Game />
+      <BrokenClick2 />
     </div>;
 
   }
